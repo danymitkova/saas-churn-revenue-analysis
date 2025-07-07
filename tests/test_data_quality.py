de6df7b55e7b6ca3627f7673d13b561d48dc6559
Dany Mitkova <danymitkovap@gmail.com>
2025-07-07 13:09:41 +0300

    Update test_data_quality.py

--- tests/test_data_quality.py
+++ tests/test_data_quality.py
@@ -1,4 +1,9 @@
 import pandas as pd
-def test_customer_ids_unique():
-    df = pd.read_csv("data/raw/customers.csv")
+from pathlib import Path
+
+ROOT = Path(__file__).parents[1]           # коренът на репото
+DATA = ROOT / "saas_churn_project" / "data" / "raw"
+
+def test_unique_customer_ids():
+    df = pd.read_csv(DATA / "customers.csv")
     assert df["customer_id"].is_unique
